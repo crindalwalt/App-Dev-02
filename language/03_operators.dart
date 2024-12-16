77d4d576eb75ed3d875447da290352421ab13ab4
Shahzad Farooq <shahzadfarooqdev@gmail.com>
2024-12-16 14:59:38 +0500

    operators done

---
+++ language/03_operators.dart
@@ -0,0 +1,51 @@
+void main() {
+  int num1 = 12;
+  int num2 = 34;
+  int sum = num1 + num2;
+  int minus = num2 - num1;
+  int multiply = num1 * num2;
+  double devide = num1 / num2;
+  // print(devide);
+
+  bool check = num1 > num2;
+  check = num2 < num1;
+  check = num2 <= num1;
+  check = num1 <= num1;
+  check = num1 >= 34;
+  check = num1 == num1;
+  check = num1 != num2;
+
+  // print(check);
+
+  int a = 45;
+  int b = 20;
+  a = b;
+  // b = 2;
+
+  a = a + 40;
+  a += 40;
+  a -= 50;
+  a *= 10;
+  double ty = 20;
+  ty /= 2;
+  // print(ty);
+  // print(b);
+
+  // local operators
+  a = 19;
+  bool condition = a < 20 || a == 18;
+  print(!condition);
+}
+
+
+/**
+ * Operators
+ *  
+ *  => TYPES
+ *    1) arithmetic operators (+,-,*,/,%)
+ *    2) camparision operators (<,>,>=,<=,==,!=)
+ *    3) Assignement operators (=,+=,-=,/=,*=)
+ *    4) Logical operators (&&,||,!)
+ * 
+ * 
+ */

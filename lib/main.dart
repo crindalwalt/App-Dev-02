977920b24106a08246be7a4a986b36ba0039ca5d
Shahzad Farooq <shahzadfarooqdev@gmail.com>
2025-01-18 18:35:44 +0500

    listview done

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class RootApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: StoryScreen(),
+      home: HomeScreen(),
     );
   }
 }
--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -6,7 +6,7 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("DreamyPages"),
-        backgroundColor: Colors.lime,
+        backgroundColor: Colors.blue,
         leading: Icon(Icons.home),
         centerTitle: true,
         actions: const [
@@ -16,19 +16,143 @@ class HomeScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Center(
-        child: Container(
-          width: 150,
-          height: 150,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(12),
-            color: Colors.tealAccent,
-            image: DecorationImage(
-              image: AssetImage(urlImage),
-              fit: BoxFit.cover,
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 200,
+            padding: EdgeInsets.all(12),
+            color: Colors.blueGrey,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                Container(
+                  margin: EdgeInsets.all(5),
+                  width: 100,
+                  height: 140,
+                  color: Colors.black,
+                ),
+                Container(
+                  width: 100,
+                  height: 140,
+                  color: Colors.orange,
+                  margin: EdgeInsets.all(5),
+                ),
+                Container(
+                  width: 100,
+                  height: 140,
+                  color: Colors.green,
+                  margin: EdgeInsets.all(5),
+                ),
+                Container(
+                  width: 100,
+                  height: 140,
+                  color: Colors.white,
+                  margin: EdgeInsets.all(5),
+                ),
+                Container(
+                  width: 100,
+                  height: 140,
+                  color: Colors.red,
+                  margin: EdgeInsets.all(5),
+                ),
+                Container(
+                  width: 100,
+                  height: 140,
+                  color: Colors.pink,
+                  margin: EdgeInsets.all(5),
+                ),
+                Container(
+                  width: 100,
+                  height: 140,
+                  color: Colors.purple,
+                  margin: EdgeInsets.all(5),
+                ),
+              ],
             ),
           ),
-        ),
+          Container(
+            width: double.infinity,
+            height: 135,
+            padding: EdgeInsets.all(12),
+            color: Colors.green,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                Container(
+                  margin: EdgeInsets.all(5),
+                  width: 100,
+                  height: 100,
+                 
+                  decoration: BoxDecoration(
+                     color: Colors.black,
+                      borderRadius: BorderRadius.circular(50)
+                  ),
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                 
+                  decoration: BoxDecoration(
+                     color: Colors.black,
+                      borderRadius: BorderRadius.circular(50)
+                  ),
+                  margin: EdgeInsets.all(5),
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                 
+                  decoration: BoxDecoration(
+                     color: Colors.black,
+                      borderRadius: BorderRadius.circular(50)
+                  ),
+                  margin: EdgeInsets.all(5),
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                 
+                  decoration: BoxDecoration(
+                     color: Colors.black,
+                      borderRadius: BorderRadius.circular(50)
+                  ),
+                  margin: EdgeInsets.all(5),
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                 
+                  decoration: BoxDecoration(
+                     color: Colors.black,
+                      borderRadius: BorderRadius.circular(50)
+                  ),
+                  margin: EdgeInsets.all(5),
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                 
+                  decoration: BoxDecoration(
+                     color: Colors.black,
+                      borderRadius: BorderRadius.circular(50)
+                  ),
+                  margin: EdgeInsets.all(5),
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                 
+                  decoration: BoxDecoration(
+                     color: Colors.black,
+                      borderRadius: BorderRadius.circular(50)
+                  ),
+                  margin: EdgeInsets.all(5),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
